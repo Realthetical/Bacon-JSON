1f5083dd79f2c6a75ea7ff8572320e0d7ad7c325
BuildTools <unconfigured@null.spigotmc.org>
2020-02-16 05:48:49 -0600

    Add project files.

---
+++ BacoNetworksJSONWriter/MainForm.cs
@@ -0,0 +1,169 @@
+﻿using Ionic.Zip;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BacoNetworksJSONWriter
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            createJSONButton.Enabled = false;
+
+        }
+
+        private void MainForm_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        string CompleteJSON = string.Empty;
+
+        private void MainForm_DragDrop(object sender, DragEventArgs e)
+        {
+            if(githubBox.Text == "put-your-github-link-here")
+            {
+                MessageBox.Show("you need to put your link in first");
+                return;
+            }
+
+
+            DataSet testSet = new DataSet("testSet");
+            testSet.Namespace = "NetFrameWork";
+            DataTable table = new DataTable();
+            table.TableName = "modules";
+            DataColumn idColumn = new DataColumn("id", typeof(string));
+            DataColumn nameColumn = new DataColumn("name", typeof(string));
+            DataColumn typeColumn = new DataColumn("type", typeof(string));
+            DataColumn itemColumn = new DataColumn("item");
+            DataColumn artifactColumn = new DataColumn("artifact", typeof(Dictionary<object, object>));
+            
+            //idColumn.AutoIncrement = true;
+
+            
+            table.Columns.Add(idColumn);
+            table.Columns.Add(nameColumn);
+            table.Columns.Add(typeColumn);
+            table.Columns.Add(itemColumn);
+            table.Columns.Add(artifactColumn);
+            testSet.Tables.Add(table);
+
+            string[] FileList = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+            foreach (string files in FileList)
+            {
+                using (ZipFile zip = ZipFile.Read(files))
+                {
+                    zip.ExtractSelectedEntries("mcmod.info", ExtractExistingFileAction.OverwriteSilently);
+                    string firstRead = File.ReadAllText("mcmod.info");
+                    string parseMe = firstRead.Substring(1, firstRead.Length - 3);
+                    //System.Threading.Thread.Sleep(50);
+                    try
+                    {
+                        var jRead = JsonConvert.DeserializeObject<dynamic>(parseMe);
+                        string name = jRead.name.ToString();
+                        string modid = jRead.modid.ToString();
+                        string modFile = System.IO.Path.GetFileName(files);
+                        modList.Items.Add(modFile);
+                        FileInfo info = new FileInfo(files);
+
+                        int size = (int)info.Length;
+                        string MD5 = CalculateMD5(files);
+
+
+                        //MessageBox.Show(parseMe);
+
+                        Dictionary<object, object> Dict = new Dictionary<object, object>
+                {
+                    { "size", size },
+                    { "MD5", MD5 },
+                    { "url", githubBox.Text + modFile}
+                };
+
+                        DataRow newRow = table.NewRow();
+                        newRow["id"] = modid;
+                        newRow["name"] = name;
+                        newRow["type"] = "ForgeMod";
+                        newRow["item"] = "item " + 1;
+                        newRow["artifact"] = Dict;
+                        table.Rows.Add(newRow);
+                    }
+                    catch
+                    {
+
+                    }
+                    
+                }
+                
+            }
+
+            testSet.AcceptChanges();
+
+            CompleteJSON = JsonConvert.SerializeObject(testSet, Formatting.Indented);
+
+            //textBox1.Text = CompleteJSON;
+
+            createJSONButton.Enabled = true;
+        }
+
+        string CalculateMD5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "");
+                }
+            }
+        }
+
+        private void deserialiseJSON(string strJSON)
+        {
+            try
+            {
+                var jRead = JsonConvert.DeserializeObject<dynamic>(strJSON);
+                MessageBox.Show(jRead.name);
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        private void createJSONButton_Click(object sender, EventArgs e)
+        {
+            if ((textBox2.Text != "") && (CompleteJSON != ""))
+            {
+                File.WriteAllText(textBox2.Text + ".json", CompleteJSON);
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //oops
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            createJSONButton.Enabled = true;
+        }
+    }
+}
---
+++ BacoNetworksJSONWriter/Mainform.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace BacoNetworksJSONWriter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.createJSONButton = new System.Windows.Forms.Button();
+            this.modList = new System.Windows.Forms.ListBox();
+            this.savePrompt = new System.Windows.Forms.SaveFileDialog();
+            this.githubBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // createJSONButton
+            // 
+            this.createJSONButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.createJSONButton.Location = new System.Drawing.Point(223, 308);
+            this.createJSONButton.Name = "createJSONButton";
+            this.createJSONButton.Size = new System.Drawing.Size(169, 68);
+            this.createJSONButton.TabIndex = 0;
+            this.createJSONButton.Text = "SAVE";
+            this.createJSONButton.UseVisualStyleBackColor = true;
+            this.createJSONButton.Click += new System.EventHandler(this.createJSONButton_Click);
+            // 
+            // modList
+            // 
+            this.modList.FormattingEnabled = true;
+            this.modList.Location = new System.Drawing.Point(13, 38);
+            this.modList.Name = "modList";
+            this.modList.Size = new System.Drawing.Size(380, 264);
+            this.modList.TabIndex = 1;
+            // 
+            // githubBox
+            // 
+            this.githubBox.Location = new System.Drawing.Point(13, 12);
+            this.githubBox.Name = "githubBox";
+            this.githubBox.Size = new System.Drawing.Size(379, 20);
+            this.githubBox.TabIndex = 2;
+            this.githubBox.Text = "put-your-github-link-here";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 308);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "What to name the JSON file?";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(13, 340);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(199, 26);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 381);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.githubBox);
+            this.Controls.Add(this.modList);
+            this.Controls.Add(this.createJSONButton);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "BacoNetworks JSON Writer";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.MainForm_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button createJSONButton;
+        private System.Windows.Forms.ListBox modList;
+        private System.Windows.Forms.SaveFileDialog savePrompt;
+        private System.Windows.Forms.TextBox githubBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
